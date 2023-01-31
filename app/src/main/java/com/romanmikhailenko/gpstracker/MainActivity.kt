98ed15169cf50c135f1fe2105a4e82bf3b809e4b
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-01-31 15:18:54 +0600

    bottom menu is added

--- app/src/main/java/com/romanmikhailenko/gpstracker/MainActivity.kt
+++ app/src/main/java/com/romanmikhailenko/gpstracker/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.romanmikhailenko.gpstracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.romanmikhailenko.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onBottomNavClicks()
+    }
+
+    private fun onBottomNavClicks(){
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.id_home -> Toast.makeText(this, "1", Toast.LENGTH_LONG).show()
+                R.id.id_tracks -> Toast.makeText(this, "2", Toast.LENGTH_LONG).show()
+                R.id.id_settings -> Toast.makeText(this, "3", Toast.LENGTH_LONG).show()
+            }
+            true
+        }
     }
 }
